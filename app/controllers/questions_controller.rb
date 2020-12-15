97c3e79a69123188903edcb70df5ea536e36531e
Shunta Nagai <shun-p@ShuntanoMacBook-Pro.local>
2020-12-16 01:07:00 +0900

    fix view and controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,38 @@ class QuestionsController < ApplicationController
   end
 
   def get_shoes
-    
+    @question = Question.new(question_params)
+    q = 'select image from Questions where usage'
+
+    if @question.foot_type != "" 
+      q + 'and foot_type = #{Question.foot_type}'
+    end
+
+    if @question.company != "" 
+      q + 'and company = #{Question.company}'
+    end
+
+    if @question.price != "" 
+      q + 'and price = #{Question.price}'
+    end
+
+    if @question.cushion != "" 
+      q + 'and cushion = #{Question.cushion}'
+    end
+
+    if @question.fashion != "" 
+      q + 'and fashion = #{Question.fashion}'
+    end
+
+    ActiveRecord::Base.connection.select_all(q)
+
+  end
+
+  def new
+    @question = Question.new
+  end
+  
+  def question_params
+    params.permit(:usage, :foot_type, :company, :price, :cushion, :fashion, :image)
   end
 end
