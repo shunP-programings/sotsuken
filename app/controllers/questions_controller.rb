f4e6098299461be6187244b60bf351be0eecea19
Shunta Nagai <shun-p@ShuntanoMacBook-Pro.local>
2020-12-18 23:11:35 +0900

    fix latest

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,32 +3,38 @@ class QuestionsController < ApplicationController
   end
 
   def get_shoes
-    @question = Question.new(question_params)
-    #@question = Question.find(params[:usage])
-    # q = "select image from questions where usage = #{@Question.usage}"
+    # @question = question_params
+    #q = "select image from questions where usage = \"#{@question["usage"]}\""
+    # ActiveRecord::Base.transaction do
+        @question = Question.select(1)
 
-    # if @question.foot_type != "" 
-    #   q + "and foot_type = #{@Question.foot_type}"
+    # end
+    # if @question["foot_type"] != "" 
+    #   q + "and foot_type = #{@question["foot_type"]}"
     # end
 
-    # if @question.company != "" 
-    #   q + "and company = #{@Question.company}"
+    # if @question["company"] != "" 
+    #   q + "and company = #{@question["company"]}"
     # end
 
-    # if @question.price != "" 
-    #   q + "and price = #{@Question.price}"
+    # if @question["price"] != "" 
+    #   q + "and price = #{@question["price"]}"
     # end
 
-    # if @question.cushion != "" 
-    #   q + "and cushion = #{@Question.cushion}"
+    # if @question["cushion"] != "" 
+    #   q + "and cushion = #{@question["cushion"]}"
     # end
 
-    # if @question.fashion != "" 
-    #   q + "and fashion = #{@Question.fashion}"
+    # if @question["fashion"] != "" 
+    #   q + "and fashion = #{@question["fashion"]}"
     # end
 
     # ActiveRecord::Base.connection.select_all(q)
+    # con = ActiveRecord::Base.connection
 
+    # question = con.select_all(q)
+    # return @question
+    # logger.debug(@question = result.to_hash)
   end
 
   def new
@@ -38,4 +44,8 @@ class QuestionsController < ApplicationController
   def question_params
     params.permit(:usage, :foot_type, :company, :price, :cushion, :fashion, :image)
   end
+
+  # def edit
+  #   render "qiestions/#{params[:]}"
+  # end
 end
